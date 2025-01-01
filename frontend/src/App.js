d7c8e96dec3e3904271c4832c3c2577f02838c76
IT <madhurajangle2004@gmail.com>
2025-01-01 16:11:54 +0530

    pycache removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
+import Home from './pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AutoTranslate from './components/test';
+import Login from './pages/Login';
+import UserSignUp from './pages/sign-up';
 function App() {
   return (
     <div className="App">
+      
       <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<UserSignUp />} />
+                <Route path="/translate" element={<AutoTranslate/>}/>
             </Routes>
         </BrowserRouter>
     </div>
