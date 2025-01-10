5e216152ebbed8783eae0714a7bd1e64e65944e5
rujutamedhi <rujutamedhi@gmail.com>
2025-01-10 13:11:48 +0530

    eligibility checked

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 from . import views
-from .views import UpdateRatingView,EligibilityCheckView
+from .views import UpdateRatingView,EligibilityCheckView,SchemeEligibilityView
 urlpatterns = [
     # path('recommend/<str:email>/', views.GetRecommendation.as_view(), name='recommendations'),
     path('update-rating/', UpdateRatingView.as_view(), name='update_rating'),
     path('create-rating/', views.CreateRatingView.as_view(), name='create-rating'),
     path('eligibility-check/<str:user_email>/', EligibilityCheckView.as_view(), name='check_eligibility'),
+    path('scheme/<str:user_email>/<str:schemename>/', SchemeEligibilityView.as_view(), name='scheme_eligibility'),
 ]
